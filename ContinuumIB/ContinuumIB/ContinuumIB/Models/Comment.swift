095353014695b0d556f093dca75ad54a4e168e53
Ian Becker <ian-becker@lambdastudents.com>
2020-08-18 13:43:57 -0700

    Models complete

---
+++ ContinuumIB/ContinuumIB/ContinuumIB/Models/Comment.swift
@@ -0,0 +1,25 @@
+//
+//  Comment.swift
+//  ContinuumIB
+//
+//  Created by Ian Becker on 8/18/20.
+//  Copyright © 2020 Ian Becker. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class Comment {
+    
+    var text: String
+    var timestamp: Date
+    weak var post: Post?
+    
+    init(text: String, timestamp: Date = Date(), post: Post) {
+        
+        self.text = text
+        self.timestamp = timestamp
+        self.post = post
+        
+    }
+}
--- ContinuumIB/ContinuumIB/ContinuumIB/Models/Post.swift
+++ ContinuumIB/ContinuumIB/ContinuumIB/Models/Post.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2020 Ian Becker. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CloudKit
+
+class Post {
+    
+    var photoData: Data?
+    var timestamp: Date
+    var caption: String
+    var comments: [Comment]
+    
+    var photo: UIImage? {
+        get {
+            guard let photoData = photoData else {return nil}
+            return UIImage(data: photoData)
+        } set {
+            photoData = newValue?.jpegData(compressionQuality: 0.5)
+        }
+    }
+    
+    init(timestamp: Date = Date(), caption: String, comments: [Comment] = [], photo: UIImage) {
+        
+        self.timestamp = timestamp
+        self.caption = caption
+        self.comments = comments
+        self.photo = photo
+        
+    }
+}
